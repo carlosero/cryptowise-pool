0cc90450ca032d0ca9e3684963bab545b14dceea
Carlos Rodriguez <carlosr706@gmail.com>
2018-03-01 21:37:00 -0500

    Working test token and manipulating tokens

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -1,45 +1,52 @@
 pragma solidity ^0.4.17;
 
+contract TestToken {
+    function balanceOf(address tokenOwner) public view returns (uint256 balance);
+}
+
 contract Manager {
     address owner;
-    uint totalTokens;
-    uint pledgedAmount;
-    mapping (address => uint) contributions;
+    TestToken tokenContractAddress; // token address of received tokens
+    uint256 tokenDecimals; // decimals of smart contract
+    uint256 totalTokens;
+    uint256 pledgedAmount;
+    mapping (address => uint256) contributions;
 
-    event Log(uint n);
+    event Log(uint256 n);
 
     function Manager() public {
         owner = msg.sender;
     }
 
-    function setContributions(address[] addresses, uint[] amounts) public {
+    function setContributions(address[] addresses, uint256[] amounts) public {
         assert(msg.sender == owner); // only owner can set contributions
         assert(addresses.length == amounts.length);
-        for (uint i = 0; i <= addresses.length-1; i++) {
+        for (uint256 i = 0; i <= addresses.length-1; i++) {
             pledgedAmount += amounts[i] * 1 ether ;
             setContribution(addresses[i], amounts[i] * 1 ether);
         }
         Log(pledgedAmount);
     }
 
-    function setContribution(address _address, uint _amount) internal {
+    function setContribution(address _address, uint256 _amount) internal {
         contributions[_address] = _amount;
     }
 
-    function getContribution(address contributor) public view returns (uint) {
+    function getContribution(address contributor) public view returns (uint256) {
         return contributions[contributor];
     }
 
-    function contributionPercentage(address contributor) public view returns (uint) {
-        return getContribution(contributor) / pledgedAmount;
+    function configureTokens(address _address, uint256 decimals) public {
+        tokenContractAddress = TestToken(_address);
+        tokenDecimals = decimals;
+        totalTokens = tokenContractAddress.balanceOf(address(this));
+        Log(totalTokens);
     }
+    function getTokensAmount() public view returns (uint256) { return totalTokens; }
 
-    function setTokensReceived(uint amount, uint decimals) public { totalTokens = amount * 10**decimals; }
-    function getTokensAmount() public view returns (uint) { return totalTokens; }
+    function getPledgedAmount() public view returns (uint256) { return pledgedAmount; }
 
-    function getPledgedAmount() public view returns (uint) { return pledgedAmount; }
-
-    function getTokens(address contributor) public view returns (uint) {
+    function getTokens(address contributor) public view returns (uint256) {
         return ((getContribution(contributor) * totalTokens) / pledgedAmount);
     }
 }
---
+++ contracts/TestToken.sol
@@ -0,0 +1,88 @@
+pragma solidity ^0.4.17;
+
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
+
+contract TestToken {
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
+
+    // This creates an array with all balances
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * Constructor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function TestToken(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balances[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+    }
+
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balances[_from] >= _value);
+        // Check for overflows
+        require(balances[_to] + _value > balances[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balances[_from] + balances[_to];
+        // Subtract from the sender
+        balances[_from] -= _value;
+        // Add the same to the recipient
+        balances[_to] += _value;
+        Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balances[_from] + balances[_to] == previousBalances);
+    }
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint256 balance) {
+        return balances[tokenOwner];
+    }
+}
