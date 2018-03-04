66f4e8a9c25a32686fa8923e00c1f463f4c0196d
Carlos Rodriguez <carlosr706@gmail.com>
2018-03-04 12:26:23 -0500

    Keep track of entire amount contributed to pool

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -9,22 +9,29 @@ contract Manager {
     address[] contributors;
     mapping (address => uint256) contributions;
 
-    event Contributed(address _address, uint256 _amount)
-    event Withdrawed(address _address, uint256 _amount)
+    // generals
+    uint256 poolContribution;
+
+    //I should see entire contribution of pool
+
+    event Contributed(address _address, uint256 _amount);
+    event Withdrawed(address _address, uint256 _amount);
 
     function Manager() public {
         owner = msg.sender;
     }
 
     function () public payable { // default action is contribute
-        contributors.push(msg.sender)
-        contributions[msg.sender] += msg.value
-        emit Contributed(msg.sender, msg.value)
+        contributors.push(msg.sender);
+        contributions[msg.sender] += msg.value;
+        poolContribution += msg.value;
+        emit Contributed(msg.sender, msg.value);
     }
 
     function withdraw(uint256 _amount) {
-        assert(contributions[msg.sender] > _amount)
-        contributions[msg.sender] = SafeMath.sub(contributions[msg.sender], _amount)
-        emit Withdrawed(msg.sender, _amount)
+        assert(contributions[msg.sender] > _amount);
+        contributions[msg.sender] = SafeMath.sub(contributions[msg.sender], _amount);
+        poolContribution -= msg.value
+        emit Withdrawed(msg.sender, _amount);
     }
 }
