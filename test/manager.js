4313ae12922dc40f5f3786375b8c8f96c975d616
Carlos Rodriguez <carlosr706@gmail.com>
2018-03-04 17:31:32 -0500

    More refactors, test that user actually receives money in account

--- test/manager.js
+++ test/manager.js
@@ -9,33 +9,41 @@ contract('manager', async (accounts)  => {
 		this.admins = [accounts[4], accounts[5], accounts[6]];
 	})
 	context('as contributor', async ()  => {
-		it("should allow me to send ether to it", async ()  => {
+		it("should allow me to contribute ether", async ()  => {
 			await this.instance.sendTransaction({ value: 12345, from: this.investors[0] });
             let balance = await this.instance.getContribution.call({from: this.investors[0]});
 			assert.equal(balance.valueOf(), 12345);
 		});
 
-		it("should allow me to withdraw the ether I sent", async ()  => {
-			await this.instance.sendTransaction({value: 12345, from: this.investors[1] });
-            await this.instance.withdrawContribution({from: this.investors[1]});
-            let balance = await this.instance.getContribution.call({from: this.investors[1]});
+		it("should allow me to withdraw the ether I contributed", async ()  => {
+			investorBalance = web3.eth.getBalance(this.investors[0]).valueOf();
+            let res = await this.instance.withdrawContribution({from: this.investors[0]});
+            gas = res.receipt.gasUsed * 100000000000;
+            newBalance = web3.eth.getBalance(this.investors[0]).valueOf();
+            assert.equal(newBalance, investorBalance-gas+12345)
+            let balance = await this.instance.getContribution.call({from: this.investors[0]});
             assert.equal(balance.valueOf(), 0);
 		});
 
+		it("should allow me to contribute again after withdrawal", async ()  => {
+			await this.instance.sendTransaction({ value: 12345, from: this.investors[0] });
+            let balance = await this.instance.getContribution.call({from: this.investors[0]});
+			assert.equal(balance.valueOf(), 12345);
+		});
+
 		it("should not allow me to withdraw more than the ether I sent", async ()  => {
-			await this.instance.sendTransaction({value: 12345, from: this.investors[1] });
-            await this.instance.withdrawContribution({from: this.investors[1]});
-         	await expectThrow(this.instance.withdrawContribution({from: this.investors[1]}), "Error");
+            await this.instance.withdrawContribution({from: this.investors[0]});
+         	await expectThrow(this.instance.withdrawContribution({from: this.investors[0]}), "Error");
 		});
 
 		it("should not allow me to modify admins", async ()  => {
-         	await expectThrow(this.instance.setAdmins(this.admins, {from: this.investors[1]}), "Error");
+         	await expectThrow(this.instance.setAdmins(this.admins, {from: this.investors[0]}), "Error");
 		});
 
 		it("should not allow me to send contribution of pool to X address", async ()  => {
 			let icoAddress = '0x123306090abab3a6e1400e9345bc60c78a8bef57';
-			await this.instance.sendTransaction({value: 12345, from: this.investors[2]});
-         	await expectThrow(this.instance.sendContribution(icoAddress, {from: this.investors[2]}), "Error");
+			await this.instance.sendTransaction({value: 12345, from: this.investors[0]});
+         	await expectThrow(this.instance.sendContribution(icoAddress, {from: this.investors[0]}), "Error");
 		});
 	});
 
@@ -55,15 +63,16 @@ contract('manager', async (accounts)  => {
 		it('should set myself as admin when I set admins', async ()  => {
 			await this.instance.setAdmins(this.admins, {from: this.owner});
 			assert.equal(await this.instance.admins.call(this.admins.length), this.owner);
+			assert.equal(await this.instance.isAdmin.call(this.owner), true);
 		});
 	});
 
 	context('as admin', async ()  => {
 		it("should allow me to send contribution of pool to X address", async ()  => {
 			let icoAddress = '0x123306090abab3a6e1400e9345bc60c78a8bef57';
-			await this.instance.sendTransaction({value: 12345, from: this.owner});
+			await this.instance.sendTransaction({value: 12345, from: this.investors[2]});
 			let poolContribution = await this.instance.poolContribution.call();
-			await this.instance.sendContribution(icoAddress, {from: this.owner});
+			await this.instance.sendContribution(icoAddress, {from: this.admins[2]});
 			assert.equal(web3.eth.getBalance(icoAddress).valueOf(), poolContribution.valueOf());
 		});
 	});
