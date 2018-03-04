cabf34921522765350606e4f683c19eff5b9eb0d
Carlos Rodriguez <carlosr706@gmail.com>
2018-03-04 13:21:24 -0500

    Implement send pool contribution to ico address

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -11,24 +11,26 @@ contract Manager {
 
     // admins
     address[] admins;
-    mapping (address => bool) isAdminOfPool;
+    mapping (address => bool) isAdmin;
 
     // generals
     uint256 poolContribution;
 
     event Contributed(address _address, uint256 _amount);
     event Withdrawed(address _address, uint256 _amount);
+    event PoolContributionSent(address _to, uint256 _amount);
 
     function Manager(address[] _admins) public {
         owner = msg.sender;
         admins = _admins;
         for (uint i = 0; i < _admins.length; i++) {
-            isAdminOfPool[_admins[i]] = true
+            isAdmin[_admins[i]] = true;
         }
     }
 
-    modifier isAdmin { require(isAdminOfPool[msg.sender]); _; }
+    modifier onlyAdmin { require(isAdmin[msg.sender]); _; }
 
+    // contributes
     function () public payable { // default action is contribute
         contributors.push(msg.sender);
         contributions[msg.sender] += msg.value;
@@ -36,10 +38,19 @@ contract Manager {
         emit Contributed(msg.sender, msg.value);
     }
 
-    function withdraw(uint256 _amount) {
+    // withdraws contribution
+    function withdraw(uint256 _amount) public {
         assert(contributions[msg.sender] > _amount);
         contributions[msg.sender] = SafeMath.sub(contributions[msg.sender], _amount);
-        poolContribution -= msg.value
+        poolContribution -= msg.value;
+        this.transfer(msg.sender, _amount);
         emit Withdrawed(msg.sender, _amount);
+    }
+
+    // sends contribution to ICO/presale address
+    function sendContribution(address _to) public onlyAdmin {
+        assert(this.balance >= poolContribution);
+        this.transfer(_to, poolContribution);
+        emit PoolContributionSent(_to, poolContribution);
     }
 }
