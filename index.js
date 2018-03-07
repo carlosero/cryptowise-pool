397800017d991b80a1152230e6bb3eaf2c132681
Carlos Rodriguez <carlosr706@gmail.com>
2018-03-07 08:33:39 -0500

    Future view

---
+++ index.js
@@ -0,0 +1,5 @@
+// Build webserver with:
+// - Allows me to create a contract (shows entire bytecode).
+// - Allows me to call it's multiple functions and params (shows entire abi for MEW).
+// - For interacting, gives me a link for pre-filling MEW.
+// - Shows me contract info.
