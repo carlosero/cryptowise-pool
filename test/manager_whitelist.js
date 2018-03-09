7a9cf15729df3133504586c235788a7fa7987e4a
Carlos Rodriguez <carlosr706@gmail.com>
2018-03-09 08:35:31 -0500

    WIP whitelist tests

--- test/manager_whitelist.js
+++ test/manager_whitelist.js
@@ -1,18 +1,37 @@
 var expectThrow = require('./helpers/expectThrow');
 var Manager = artifacts.require("./Manager.sol");
 
+async function expectCanInvest(instance, investor) {
+	await instance.sendTransaction({ value: 12340000, from: investor });
+	assert.equal((await instance.contributions.call(investor)).valueOf(), 12340000);
+}
+
+async function expectCanNotInvest(instance, investor) {
+	await expectThrow(instance.sendTransaction({ value: 12340000, from: investor }));
+	assert.equal((await instance.contributions.call(investor)).valueOf(), 0);
+}
+
 contract('manager whitelist workflow functionality', async (accounts)  => {
 	before(async () => {
 		this.owner = accounts[0];
-		this.investors = [accounts[1], accounts[2], accounts[3]];
-		this.admins = [accounts[4], accounts[5], accounts[6]];
-		this.instance = await Manager.new(300, 0, 0, 0, false, false, [this.admins[0], this.admins[1], this.admins[2]]);
-		this.tokenContract = await TestToken.new(100000000, 'SomeEthereumToken', 3, 'SET');
-		this.icoAddress = this.tokenContract.address;
+		this.investors = [accounts[1], accounts[2], accounts[3], accounts[4]];
+		this.admins = [accounts[5], accounts[6]];
+		this.instance = await Manager.new(300, 0, 0, 0, false, false, this.admins);
 	});
-
-	it("allows only whitelist investors to contribute");
-	it("does not allows normal people to contribute");
+	it('should allow admin to setup whitelist', async () => {
+		await this.instance.setupWhitelist(true, [this.investors[0]]);
+		expectCanInvest(this.instance, this.investors[0]);
+		expectCanNotInvest(this.instance, this.investors[3]);
+	});
+	it('should allow admin to add individuals to whitelist', async () => {
+		await this.instance.addToWhitelist(this.investors[1]);
+		expectCanInvest(this.instance, this.investors[1]);
+	});
+	it('should allow admin to remove individuals from whitelist', async () => {
+		await this.instance.removeFromWhitelist(this.investors[0]);
+		expectCanInvest(this.instance, this.investors[1]);
+		expectCanNotInvest(this.instance, this.investors[0]);
+	});
 });
 
 contract('manager blacklist workflow functionality', async (accounts) => {
